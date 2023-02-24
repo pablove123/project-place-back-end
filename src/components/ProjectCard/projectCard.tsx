01336afbd97ef81720c88cb3febbd79fc91f3b6c
Pablo Villedae <pablov.e12345@gmail.com>
2023-02-24 13:13:39 -0800

    add projectProps to projectList

---
+++ src/components/ProjectCard/projectCard.tsx
@@ -0,0 +1,7 @@
+const ProjectCard = () => {
+  return ( 
+    <h1>This is project Card</h1>
+  );
+}
+
+export default ProjectCard;
--- src/components/Projects/ProjectList.tsx
+++ src/components/Projects/ProjectList.tsx
@@ -1,9 +1,18 @@
-const ProjectList = (props) => {
+import { Project } from "../../types/models";
+import ProjectCard from "../ProjectCard/projectCard";
+
+interface ProjectProps{
+  projects: Project[]
+}
+const ProjectList = (props:ProjectProps) => {
   console.log(props)
   return (
+    <>
     <h1>Project List</h1>
+    <ProjectCard/>
+    </>
     );
 
 }
- 
+
 export default ProjectList;
